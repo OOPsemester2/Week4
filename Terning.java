3a0c7a613023579d20df90774541bc75f1e0b186
nico4925 <s174872@student.dtu>
2018-02-19 15:17:52 +0100

    UTF-8 error edit

--- Terning.java
+++ Terning.java
@@ -1,41 +1,36 @@
 package kapitel_04;
+
 /** En klasse der beskriver 6-sidede terninger */
-public class Terning
-{
-	/** antallet af øjne på den side på terningen, der vender opad lige nu */
-	private int værdi;
+public class Terning {
+	/** antallet af øjne paa den side paa terningen, der vender opad lige nu */
+	private int vaerdi;
 
 	/** konstruktør der opretter en terning */
-	public Terning()
-	{
-		// vælg en tilfældig side til at starte med
-		værdi = (int) (Math.random() * 6 + 1);
+	public Terning() {
+		// vaelg en tilfaeldig side til at starte med
+		vaerdi = (int) (Math.random() * 6 + 1);
 	}
 
-	/** kaster terningen, så den får en anden værdi */
-	public void kast()
-	{
-		// vælg en tilfældig side
-		double tilfældigtTal = Math.random();
-		værdi = (int) (tilfældigtTal * 6 + 1);
+	/** kaster terningen, saa den faar en anden vaerdi */
+	public void kast() {
+		// vaelg en tilfaeldig side
+		double tilfaeldigtTal = Math.random();
+		vaerdi = (int) (tilfaeldigtTal * 6 + 1);
 	}
 
-	/** Aflæser terningens værdi */
-	public int getVærdi()
-	{
-		return værdi;
+	/** Aflaeser terningens vaerdi */
+	public int getVaerdi() {
+		return vaerdi;
 	}
 
-	/** Sætter terningens værdi */
-	public void setVærdi(int nyVærdi)
-	{
-		værdi = nyVærdi;
+	/** Saetter terningens vaerdi */
+	public void setVaerdi(int nyVaerdi) {
+		vaerdi = nyVaerdi;
 	}
 
 	/** giver en beskrivelse af terningen som en streng */
-	public String toString()
-	{
-		String svar = ""+værdi;  // værdi som streng, f.eks. "4"
+	public String toString() {
+		String svar = "" + vaerdi; // vaerdi som streng, f.eks. "4"
 		return svar;
 	}
 }
