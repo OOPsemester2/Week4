ca08c05700cbd93b8a5530bdce4dec782156d5af
nico4925 <s174872@student.dtu>
2018-02-23 02:30:08 +0100

    opgav 1 - færdig

--- Boks/BenytBoks3.java
+++ Boks/BenytBoks3.java
@@ -2,8 +2,10 @@ public class BenytBoks3 {
 	public static void main(String[] arg) {
 		Boks3 enBoks = new Boks3(); // brug konstruktoeren uden parametre
 		System.out.println("Volumen er: " + enBoks.volumen());
+		System.out.println("Vaegten er: " + enBoks.Vaegten());
 
-		Boks3 enAndenBoks = new Boks3(5, 5, 10); // brug den anden konstruktoer
+		Boks3 enAndenBoks = new Boks3(5, 5, 10, 10); // brug den anden konstruktoer
 		System.out.println("Volumen er: " + enAndenBoks.volumen());
+		System.out.println("Vaegten er: " + enAndenBoks.Vaegten());
 	}
 }
--- Boks/Boks3.java
+++ Boks/Boks3.java
@@ -3,6 +3,7 @@ public class Boks3 {
   private double laengde;
   private double bredde;
   private double hoejde;
+  private double massefylde;
 
   /** konstruktoer der opretter en standardboks */
   public Boks3() {
@@ -10,25 +11,28 @@ public class Boks3 {
     laengde = 10.0;
     bredde = 10.0;
     hoejde = 10.0;
+    massefylde = 10.0;
   }
 
   /** en anden konstruktoer der faar bredde, hoejde og laengde */
-  public Boks3(double lgd, double b, double h) {
-    System.out.println("Boks oprettes med lgd=" + lgd + " b=" + b + " h=" + h);
-    saetMaal(lgd,b ,h );
+  public Boks3(double lgd, double b, double h, double m) {
+    System.out.println("Boks oprettes med lgd=" + lgd + " b=" + b + " h=" + h + " m=" + m);
+    saetMaal(lgd, b, h, m);
   }
 
   /** saetter boksens bredde, hoejde og laengde */
-  public void saetMaal(double lgd, double b, double h) {
-    if (lgd <= 0 || b <= 0 || h <= 0) {
+  public void saetMaal(double lgd, double b, double h, double m) {
+    if (lgd <= 0 || b <= 0 || h <= 0 || m <= 0) {
       System.out.println("Ugyldige maal. Bruger standardmaal.");
       laengde = 10.0;
       bredde = 10.0;
       hoejde = 10.0;
+      massefylde = 10.0;
     } else {
       laengde = lgd;
       bredde = b;
       hoejde = h;
+      massefylde = m;
     }
   }
 
@@ -36,5 +40,10 @@ public class Boks3 {
   public double volumen() {
     double vol = laengde * bredde * hoejde;
     return vol;
+  }
+
+  public double Vaegten() {
+    double masse = massefylde * (laengde * bredde * hoejde);
+    return masse;
   }
 }
