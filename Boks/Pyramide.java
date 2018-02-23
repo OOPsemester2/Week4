89da73bb40b101e5c88423c51019f9cd8e14f5f5
nico4925 <s174872@student.dtu>
2018-02-23 02:41:44 +0100

    Pyramide - core edit

--- Boks/Pyramide.java
+++ Boks/Pyramide.java
@@ -1,18 +1,18 @@
 /** En boks med en laengde, bredde og hoejde */
-public class Boks3 {
+public class Pyramide {
   private double side;
   private double hoejde;
 
   /** konstruktoer der opretter en standardboks */
-  public Boks3() {
+  public Pyramide() {
     System.out.println("Standardboks oprettes");
     side = 10.0;
-    bredde = 10.0;
+    hoejde = 10.0;
   }
 
   /** en anden konstruktoer der faar bredde, hoejde og laengde */
-  public Side(double s, double h) {
-    System.out.println("Pyramide oprettes med s=" + s + " h=" + h);
+  public Pyramide(double s, double h) {
+    System.out.println("\nPyramide oprettes med s=" + s + " h=" + h);
     saetMaal(s, h);
   }
 
