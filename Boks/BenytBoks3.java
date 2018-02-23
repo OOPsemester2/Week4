d19927cdb8589471a01effb0fe9b5a88faee64cd
nico4925 <s174872@student.dtu>
2018-02-23 02:09:01 +0100

    UTF-8 problem...

--- Boks/BenytBoks3.java
+++ Boks/BenytBoks3.java
@@ -1,11 +1,9 @@
-public class BenytBoks3
-{
-	public static void main(String[] arg)
-	{
-		Boks3 enBoks = new Boks3();             // brug konstruktøren uden parametre
-		System.out.println("Volumen er: "+ enBoks.volumen());
+public class BenytBoks3 {
+	public static void main(String[] arg) {
+		Boks3 enBoks = new Boks3(); // brug konstruktoeren uden parametre
+		System.out.println("Volumen er: " + enBoks.volumen());
 
-		Boks3 enAndenBoks = new Boks3(5, 5, 10);  // brug den anden konstruktør
-		System.out.println("Volumen er: "+ enAndenBoks.volumen());
+		Boks3 enAndenBoks = new Boks3(5, 5, 10); // brug den anden konstruktoer
+		System.out.println("Volumen er: " + enAndenBoks.volumen());
 	}
 }
--- Boks/Boks3.java
+++ Boks/Boks3.java
@@ -1,46 +1,40 @@
-/** En boks med en længde, bredde og højde */
-public class Boks3
-{
-	private double længde;
-	private double bredde;
-	private double højde;
+/** En boks med en laengde, bredde og hoejde */
+public class Boks3 {
+  private double laengde;
+  private double bredde;
+  private double hoejde;
 
-	/** konstruktør der opretter en standardboks */
-	public Boks3()
-	{
-		System.out.println("Standardboks oprettes");
-		længde = 10.0;
-		bredde = 10.0;
-		højde  = 10.0;
-	}
+  /** konstruktoer der opretter en standardboks */
+  public Boks3() {
+    System.out.println("Standardboks oprettes");
+    laengde = 10.0;
+    bredde = 10.0;
+    hoejde = 10.0;
+  }
 
-	/** en anden konstruktør der får bredde, højde og længde */
-	public Boks3(double lgd, double b, double h)
-	{
-		System.out.println("Boks oprettes med lgd="+lgd+" b="+b+" h="+h);
-		sætMål(lgd,b,h);
-	}
+  /** en anden konstruktoer der faar bredde, hoejde og laengde */
+  public Boks3(double lgd, double b, double h) {
+    System.out.println("Boks oprettes med lgd=" + lgd + " b=" + b + " h=" + h);
+    saetMaal(lgd,b ,h );
+  }
 
-	/** sætter boksens bredde, højde og længde */
-	public void sætMål(double lgd, double b, double h)
-	{
-		if (lgd<=0 || b<=0 || h<=0)
-		{
- 			System.out.println("Ugyldige mål. Bruger standardmål.");
-			længde = 10.0;
-			bredde = 10.0;
-			højde  = 10.0;
-		} else {
-			længde = lgd;
-			bredde = b;
-			højde  = h;
-		}
-	}
+  /** saetter boksens bredde, hoejde og laengde */
+  public void saetMaal(double lgd, double b, double h) {
+    if (lgd <= 0 || b <= 0 || h <= 0) {
+      System.out.println("Ugyldige maal. Bruger standardmaal.");
+      laengde = 10.0;
+      bredde = 10.0;
+      hoejde = 10.0;
+    } else {
+      laengde = lgd;
+      bredde = b;
+      hoejde = h;
+    }
+  }
 
-	/** udregner boksens rumfang */
-	public double volumen()
-	{
-		double vol = længde*bredde*højde ;
-		return vol;
-	}
+  /** udregner boksens rumfang */
+  public double volumen() {
+    double vol = laengde * bredde * hoejde;
+    return vol;
+  }
 }
